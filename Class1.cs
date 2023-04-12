ec0dfe8eeea7993564dbad501f2d5d7e12b8296d
sinegia <endibro75@gmail.com>
2023-04-12 14:38:47 +0300

    add

--- Class1.cs
+++ Class1.cs
@@ -8,7 +8,6 @@ namespace WSUniversalLib
 {
     public  class MaterialCalculator
     {
-        // Расчет количества необходимого сырья для производства
         public static int CalculateMaterials(int count, float width, float length, int productType, int materialType)
         {
             if (productType < 1 || productType > 3 || materialType < 1 || materialType > 2 || count < 0 || width < 0 || length < 0)
